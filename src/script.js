2882fd5fba4442f38966f74f476006dda59bc9c7
Marc-Julius <marcellinjulius@gmail.com>
2026-04-07 14:49:30 +0100

    Design a modal using Javascript

--- src/script.js
+++ src/script.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded" ,()=>{
     
 const dropdown = document.getElementById('dropdown');
 const button = document.querySelector('button#menu-button');
-
+const dropdown2 = document.getElementById('dropdown2');
+const button2 = document.querySelector('button#menu-button2');
 button.addEventListener("click", (event) =>{
     dropdown.classList.toggle("hidden");
 })
@@ -12,4 +13,24 @@ document.addEventListener("click", (event)=>{
         dropdown.classList.add("hidden");
     }
 })
+
+button2.addEventListener("click", (event) =>{
+    dropdown2.classList.toggle("hidden");
+})
+
+document.addEventListener("click", (event)=>{
+    if (!dropdown2.classList.contains("hidden") && !button2.contains(event.target)){
+        dropdown2.classList.add("hidden");
+    }
+})
+const modal = document.getElementById('modal');
+const openBtn = document.getElementById('open-btn');
+const closeBtn = document.getElementById('close-btn');
+
+const toggleModal = () => modal.classList.toggle('hidden');
+const toggleFlex = () => modal.classList.toggle('flex');
+
+openBtn.onclick = () => { toggleModal(); toggleFlex(); };
+closeBtn.onclick = () => { toggleModal(); toggleFlex(); };
+
 })
